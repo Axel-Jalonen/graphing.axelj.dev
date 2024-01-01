8f3b375f31198955d0a9b9cba918b55706c8a48a
MostLeVert <axeljalonen@gmail.com>
2024-01-01 15:25:53 -0800

    Refactor and optimize JS, clean CSS, add UI functionality and change UI

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 const canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const pointCount = document.getElementById("point-count");
+const equationInput = document.getElementById("equation-input");
+const pointCountInput = document.getElementById("point-count-input");
+const scaleDisplay = document.getElementById("scale-display");
 const gl = canvas.getContext("webgl");
+
+document.addEventListener("DOMMouseScroll", handleScroll);
+document.addEventListener("mousewheel", handleScroll);
+function handleScroll(event) {
+    let increment = (1 / 25) * scale;
+    const delta = Math.max(-1, Math.min(1, event.wheelDelta || -event.detail));
+    scale -= delta > 0 ? increment : -increment;
+    scaleDisplay.textContent = "Scale: " + Math.round(scale * 1000) / 1000;
+    handleNewFrame();
+}
+pointCountInput.addEventListener("input", handleNewFrame);
+equationInput.addEventListener("input", handleNewFrame);
+
+function handleNewFrame() {
+    drawNewBuffer();
+}
 
 const vertexShaderSource = `
             attribute vec2 position;
@@ -10,7 +30,6 @@ const vertexShaderSource = `
                 gl_PointSize = 5.0;
             }
         `;
-
 const fragmentShaderSource = `
             precision mediump float;
             uniform vec4 color;
@@ -22,9 +41,11 @@ const fragmentShaderSource = `
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, vertexShaderSource);
 gl.compileShader(vertexShader);
+
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, fragmentShaderSource);
 gl.compileShader(fragmentShader);
+
 const program = gl.createProgram();
 gl.attachShader(program, vertexShader);
 gl.attachShader(program, fragmentShader);
@@ -32,7 +53,6 @@ gl.linkProgram(program);
 gl.useProgram(program);
 
 const colorUniformLocation = gl.getUniformLocation(program, "color");
-
 
 const positionAttributeLocation = gl.getAttribLocation(program, "position");
 gl.enableVertexAttribArray(positionAttributeLocation);
@@ -44,7 +64,7 @@ var verticesPoints = [];
 var scale = 10;
 var count = 1;
 
-function fix(str) {
+function mapEquation(str) {
   str = str.replace(/\bsin\b/g, "Math.sin");
   str = str.replace(/\bcos\b/g, "Math.cos");
   str = str.replace(/\btan\b/g, "Math.tan");
@@ -55,49 +75,25 @@ function fix(str) {
   str = str.replace(/\bsinh\b/g, "Math.sinh");
   str = str.replace(/\bcosh\b/g, "Math.cosh");
   str = str.replace(/\btanh\b/g, "Math.tanh");
+  str = str.replace(/\babs\b/g, "Math.abs");
   str = str.replace(/\^/g, "**");
   return str;
 }
 
-function updateTing() {
+function drawNewBuffer() {
     verticesPoints.length = 0;
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(0), gl.STATIC_DRAW);
-    point_count.innerHTML = "Points: " + Math.round(2 / (1 / slider.value), 3);
-
-    for (let i = -1; i < 1; i += 1 / slider.value) {
+    pointCount.textContent = "Points: " + Math.round(2 / (1 / pointCountInput.value), 3);
+    for (let i = -1; i < 1; i += 1 / pointCountInput.value) {
         let x = i * scale;
-        y = eval(fix(eq.value));
+        var mappedFunction = mapEquation(equationInput.value)
+        y = eval(mappedFunction);
         if (!y > scale || !y < -scale || !isNaN(y)) {
             verticesPoints.push(x / scale, y / scale);
-        }
+        } 
     }
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticesPoints), gl.STATIC_DRAW);
-    console.log(verticesPoints.length)
-
-}
-updateTing();
-
-function render() {
     gl.uniform4f(colorUniformLocation, 1, 1, 1, 1);
     gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
     gl.drawArrays(gl.POINTS, 0, verticesPoints.length / 2);
-    window.requestAnimationFrame(render);
-};
-render();
-
-document.addEventListener("DOMMouseScroll", handleScroll);
-document.addEventListener("mousewheel", handleScroll);
-function handleScroll(event) {
-    let increment = (1 / 25) * scale;
-    const delta = Math.max(-1, Math.min(1, event.wheelDelta || -event.detail));
-    scale -= delta > 0 ? increment : -increment;
-    point_follower.innerHTML = "Scale: " + Math.round(scale * 1000) / 1000;
-    updateTing();
 }
-
-slider.addEventListener("input", () => {
-    updateTing();
-});
-eq.addEventListener("input", () => {
-    updateTing();
-});
